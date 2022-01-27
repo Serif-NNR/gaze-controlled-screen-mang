7def9c6098361a98b01d82e53a7d799d70dcc36b
Şerif İnanır <sheriffnnr@gmail.com>
2022-01-27 17:40:04 +0300

    Add files via upload

---
+++ Gaze-Controlled Screen-Magnifying Application/Form1.cs
@@ -0,0 +1,35 @@
+﻿using PupilCaptureOperation;
+using ScreenMagnifier;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Gaze_Controlled_Screen_Magnifying_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Dictionary<string, string> pupil_capture_properties = new Dictionary<string, string>();
+        private StartMagnifier magnifier;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitConfiguration configuration = new InitConfiguration();
+            pupil_capture_properties.Add("pupilRemoteAddress"  , configuration.pupilRemoteAddress);
+            pupil_capture_properties.Add("pupilRemotePort"     , configuration.pupilRemotePort);
+            pupil_capture_properties.Add("pupilCaptureVersion" , configuration.pupilCaptureVersion);
+            pupil_capture_properties.Add("requestMessage"      , configuration.requestMessage);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IPupilCapture pupil_capture = PupilCaptureSelection.GetPupil(pupil_capture_properties);
+            magnifier = new StartMagnifier(pupil_capture);
+        }
+    }
+}
---
+++ Gaze-Controlled Screen-Magnifying Application/InitConfiguation.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Gaze_Controlled_Screen_Magnifying_Application
+{
+    public class InitConfiguration
+    {
+        // Magnifier Configuration
+        public float magLevel = 2.0f;
+        public int scrollPercent = 10;
+        public int magUpdateDurationMilliseconds = 10;
+        public bool fShow = true;
+        public bool fullScreenMagnifier = true;
+        
+
+        // Pupil Capture Configuration
+        public string pupilRemoteAddress = "tcp://127.0.0.1:";
+        public string pupilRemotePort = "50020";
+        public string pupilCaptureVersion = "v1.1";
+        public int gazeDataHz = 30;
+        public int fixationDurationMilliseconds = 250;
+        public float confidenceAmount = 0.80f;
+        public string requestMessage = "surface";
+
+
+        // Version
+        public string version = "v2.0";
+        
+
+    }
+}
